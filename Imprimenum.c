7e26721e520b9c9a11bc44f4cf771836426a81b3
hgarcg04 <hgarcg04@estudiantes.unileon.es>
2024-11-27 19:39:56 +0100

    Add files via upload

---
+++ Imprimenum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+     int num;
+     printf("Escribe un numero... ");
+     scanf("%d",&num);
+     printf("El numero introducido es %d \n",num);
+     printf("Fin del algoritmo \n");
+     return 0;
+}
